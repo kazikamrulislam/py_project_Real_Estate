4d17d8ec7d318becc43a15a7363c78d658e0a77c
kazikamrulislam <kamrul.lab@gmail.com>
2021-04-10 02:24:50 +0600

    new commit of methods

--- listings/admin.py
+++ listings/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Listing
 
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'city', 'title', 'zipcode', 'description', 'price', 'bedrooms', 'sqft', 'state')
+    list_per_page = 25
+
+admin.site.register(Listing, ListingAdmin)
--- realtors/admin.py
+++ realtors/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from .models import Realtor
 
-admin.site.register(Realtor)
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id','name', 'phone', 'email', 'hire_date')
+    list_display_links = ('id', 'name')
+    list_per_page = 25
+    search_fields = ('name',)
+
+
+admin.site.register(Realtor, RealtorAdmin)
